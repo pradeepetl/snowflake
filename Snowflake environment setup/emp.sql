1c9377b2d990af57da32f4470c8ff90aea5f2bf8
PRADEEP HC <pradeephc067@gmail.com>
2020-07-04 06:55:24 +0530

    Employee files

---
+++ Snowflake environment setup/emp.sql
@@ -0,0 +1,8 @@
+create or replace table emp (
+         first_name string ,
+         last_name string ,
+         email string ,
+         streetaddress string ,
+         city string ,
+         start_date date
+         );
